18ef99cae26f4aedae6b8b148e25e237ba100c25
InnocentWahome <innocentwahome@gmail.com>
2022-07-01 22:44:44 +0300

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+putchar('\n');
+return (0);
+}
