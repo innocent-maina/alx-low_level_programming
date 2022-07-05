bcd2ccea7420d1b5fb5ddbdd8d040e14fec43499
InnocentWahome <innocentwahome@gmail.com>
2022-07-05 23:35:39 +0300

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,34 @@
 */
 void times_table(void)
 {
-int i;
-int j;
+	int i;
+	int j;
+	int k = 9;
 
-for (j = 0; j <= 9; j++)
-{
-for (j = 0; j <= 9; j++)
-{
-_putchar(i * j);
-_putchar(',');
-_putchar(' ');
-}
-_putchar('\n');
-}
+	for (i = 0; i <= k; i++)
+	{
+		for (j = 0; j <= k; j++)
+		{
+			if ((i * j) < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((j * i) + '0');
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar((((j * i) / 10) % 10) + '0');
+				_putchar(((j * i) % 10) + '0');
+			}
+			if (j != k)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
