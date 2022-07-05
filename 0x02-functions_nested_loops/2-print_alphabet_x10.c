aef461f6c28c59f417a8f0789cf7217356bfc652
InnocentWahome <innocentwahome@gmail.com>
2022-07-05 15:38:42 +0300

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+{
+char i;
+int j;
+for (j = 0; i <= 9;)
+{
+for (i = 'a'; i <= 'z';)
+{
+_putchar(i * 10);
+i++;
+}
+_putchar('\n');
+j++;
+}
+}
