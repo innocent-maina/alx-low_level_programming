b99396e3ca46718647bed16179a731de5ff8927d
InnocentWahome <innocentwahome@gmail.com>
2022-07-05 20:47:08 +0300

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_sign - Prints if the number is +, 0 or -
+* @n: the number being checked
+* Return: 1 if +ve, 0 if 0, -1 if -ve
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else if (n < 0)
+{
+return (-1);
+}
+}
