d8072dfd626f5e21feb6ba9e2fbbae4b50cbbdd4
InnocentWahome <innocentwahome@gmail.com>
2022-07-05 16:10:51 +0300

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * _islower - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+if ( c >= 97 || c <=  122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
