05ba5317ee3d8dc64a3c71978a9f8920f94f7813
InnocentWahome <innocentwahome@gmail.com>
2022-07-07 01:23:19 +0300

    3. Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,5 @@
+
+
 #include "main.h"
 #include <stdio.h>
 
@@ -15,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 		{
 
-		if (day > 60 || day == 60)
+		if (month > 2)
 		{
 			day++;
 		}
@@ -40,4 +42,3 @@ void print_remaining_days(int month, int day, int year)
 			}
 	}
 }
-
