68e334d663801476fddecf881309bea87d6b22f0
InnocentWahome <innocentwahome@gmail.com>
2022-07-07 23:39:38 +0300

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - takes a date and prints how many days are
+* @c: letter to check
+* Return: 1 if uppercase, 0 if otherwise
+*/
+
+int _isupper(int c)
+{
+int c;
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
