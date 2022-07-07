d3a185e20f1e0ff5a499b482e76c784d68e350b8
InnocentWahome <innocentwahome@gmail.com>
2022-07-07 23:46:13 +0300

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * _isupper - takes a date and prints how many days are
@@ -9,8 +8,7 @@
 
 int _isupper(int c)
 {
-int c;
-if (c >= 65 && c <= 90)
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
