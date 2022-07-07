2c9881e19febb328a1d183c7cd924dcfdeafea1f
InnocentWahome <innocentwahome@gmail.com>
2022-07-07 23:49:31 +0300

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - takes a date and prints how many days are
+* @c: letter to check
+* Return: 1 if digit, 0 if otherwise
+*/
+
+int _isdigit(int c)
+{
+if (c >= 1 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
