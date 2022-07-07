a12ed95708d7e8796c5e075585c1234983009b0e
InnocentWahome <innocentwahome@gmail.com>
2022-07-08 01:26:56 +0300

    remove .gitignore

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints most numbers.
- * Return: void.
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
  */
-
-
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 50 && i != 52)
-		{
-			_putchar(i);
-		}
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
