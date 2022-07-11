08cbee15c49d08259963c8b751e2ac6b822c0f66
InnocentWahome <innocentwahome@gmail.com>
2022-07-11 23:36:01 +0300

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - this is the description.
+ * @str : pointer
+ * Return: this is the return value.
+ */
+
+
+void _puts(char *str)
+{
+puts(*str);
+puts("\n");
+}
