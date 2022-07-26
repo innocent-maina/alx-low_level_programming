4f4218262c3657e71b5e116626205249096d3e35
InnocentWahome <innocentwahome@gmail.com>
2022-07-26 22:48:41 +0300

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strlen - length of a string
@@ -9,14 +10,14 @@
 
 int _strlen(char *s)
 {
-int l = 0;
+	int l = 0;
 
-while (*s != '\0')
-{
-s++;
-l++;
-}
-return (l);
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
 }
 
 /**
@@ -28,38 +29,38 @@ return (l);
 
 char *argstostr(int ac, char **av)
 {
-int i, j, k;
-int len, R = 0;
-char *p;
+	int i, j, k;
+	int len, R = 0;
+	char *p;
 
-if (!ac || !av)
-{
-return (NULL);
-}
-R = 0;
+	if (!ac || !av)
+	{
+		return (NULL);
+	}
+	R = 0;
 
-for (i = 0; i < ac; i++)
-{
-len = _strlen(av[i]) + 1;
-R += len;
-}
-p = malloc(sizeof(char) * R + 1);
+	for (i = 0; i < ac; i++)
+	{
+		len = _strlen(av[i]) + 1;
+		R += len;
+	}
+	p = malloc(sizeof(char) * R + 1);
 
-if (!p)
-{
-return (NULL);
-}
+	if (!p)
+	{
+		return (NULL);
+	}
 
-for (i = 0; i < ac; i++)
-{
-len = _strlen(av[i]);
+	for (i = 0; i < ac; i++)
+	{
+		len = _strlen(av[i]);
 
-for (j = 0; j < len; j++, k++)
-{
-p[k] = av[i][j];
-}
-p[k++] = '\n';
-}
-p[k] = '\0';
-return (p);
+		for (j = 0; j < len; j++, k++)
+		{
+			p[k] = av[i][j];
+		}
+		p[k++] = '\n';
+	}
+	p[k] = '\0';
+	return (p);
 }
