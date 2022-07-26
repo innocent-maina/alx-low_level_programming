e74133896d7ecfb1f840aff0a9603a4dc4433056
InnocentWahome <innocentwahome@gmail.com>
2022-07-26 22:21:03 +0300

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 void print_grid(int **grid, int width, int height);
+
+#endif
