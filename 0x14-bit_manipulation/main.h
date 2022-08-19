4e12ac4e4e8c01329d066636cdc7167d8eb242d1
InnocentWahome <innocentwahome@gmail.com>
2022-08-19 21:01:55 +0300

    4. 100

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,6 +10,7 @@ int _putchar(char c);
 int bin_help(unsigned long int n, unsigned long int sub);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 
 #endif
